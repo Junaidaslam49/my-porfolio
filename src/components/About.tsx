57b8c1f434abd9a42771208a2c8e82e38e7a73cf
Junaidaslam49 <junaidaslam49@yahoo.com>
2026-05-08 15:42:22 +0500

    fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { ArrowRight, Linkedin } from 'lucide-react';
+// import { ArrowRight, Linkedin } from 'lucide-react';
 
 export default function About() {
   return (
